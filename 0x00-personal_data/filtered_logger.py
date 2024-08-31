ab9ca1539c652a2894d3ee9976d0eaee21275d42
Gbolahan Akande <geakande@gmail.com>
2024-08-31 20:16:59 +0100

    Filtered Logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,26 +1,99 @@
 #!/usr/bin/env python3
 
 
-"""The Module for the filter_datum function for log obfuscation.
+"""
+Module for handling Personal Data
 """
 
 
 import re
 from typing import List
+import logging
+from os import environ
+import mysql.connector
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """Returning a log message obfuscated"""
+
+    for f in fields:
+        message = re.sub(f'{f}=.*?{separator}',
+                         f'{f}={redaction}{separator}', message)
+    return message
+
+
+def get_logger() -> logging.Logger:
+    """Returning a Logger Object"""
+
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
+    logger.addHandler(stream_handler)
+
+    return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """Returning a connector to a MySQL database"""
+
+    username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+    password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = environ.get("PERSONAL_DATA_DB_NAME")
+
+    cnx = mysql.connector.connection.MySQLConnection(user=username,
+                                                     password=password,
+                                                     host=host,
+                                                     database=db_name)
+    return cnx
+
+
+def main():
     """
-    Obfuscate specified fields in a log message.
-
-    Args:
-        fields (List[str]): List of strings representing fields to obfuscate.
-        redaction (str): String to replace sensitive information.
-        message (str): Log line to obfuscate.
-        separator (str): Character separating fields in the log line.
-
-    Returns:
-        str: The log message with specified fields obfuscated.
+    Obtain a database connection using get_db and retrieves all rows
+    in the users table and display each row under a filtered format
     """
-    pattern = rf'({"|".join(map(re.escape, fields))})\{separator}[^{separator}]+'
-    return re.sub(pattern, f'\\1{separator}{redaction}', message)
+
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    field_names = [i[0] for i in cursor.description]
+
+    logger = get_logger()
+
+    for row in cursor:
+        str_row = ''.join(f'{f}={str(r)}; ' for r, f in zip(row, field_names))
+        logger.info(str_row.strip())
+
+    cursor.close()
+    db.close()
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Filters values in incoming log records using filter_datum"""
+
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
+
+
+if __name__ == '__main__':
+    main()
