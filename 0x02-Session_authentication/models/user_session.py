304fd2e64b012eed7046b64455573ebec8f4f897
Gbolahan Akande <geakande@gmail.com>
2024-09-09 21:02:04 +0100

    Session Authentication project using python

---
+++ 0x02-Session_authentication/models/user_session.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""The session for the user module"""
+
+
+from models.base import Base
+
+
+class UserSession(Base):
+    """The class for the user session"""
+
+
+    def __init__(self, *args: list, **kwargs: dict):
+        """Method to initialize the user session"""
+
+        super().__init__(*args, **kwargs)
+        self.user_id = kwargs.get('user_id')
+        self.session_id = kwargs.get('session_id')
